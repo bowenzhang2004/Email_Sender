44878e13d2c604cc853478d1227e2f95cdfce21d
Bowen <132832360+bowenzhang2004@users.noreply.github.com>
2023-05-11 16:46:16 -0400

    Update Email Sender.py

--- Email Sender.py
+++ Email Sender.py
@@ -3,21 +3,24 @@ from email.message import EmailMessage
 from string import Template
 from pathlib import Path
 
-html = Template(Path('index.html').read_text())
+try:
+    html = Template(Path('index.html').read_text())
 
-email = EmailMessage()
-email['from'] = 'Name'
-email['to'] = 'Email'
-email['subject'] = 'Subject'
+    email = EmailMessage()
+    email['from'] = 'Name'
+    email['to'] = 'Email'
+    email['subject'] = 'Subject'
 
-email.set_content(html.substitute({'name': 'Name'}), 'html')
+    email.set_content(html.substitute({'name': 'Name'}), 'html')
 
-with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
-    smtp.ehlo()
-    smtp.starttls()
-    smtp.login('Email', 'Password')
-    smtp.send_message(email)
-    print('all set')
+    with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.login('Email', 'Password')
+        smtp.send_message(email)
+        print('all set')
+except Exception as e:
+    print(f"An error occurred: {e}")
 
 
 
